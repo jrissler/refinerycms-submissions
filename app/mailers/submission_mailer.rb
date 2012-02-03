821d87a8350d18737e6c3d1b89bf0ffe7688f5a4
James Rissler <risslerjames@gmail.com>
2012-02-03 09:35:42 -0500

    use submission instance.

--- app/mailers/submission_mailer.rb
+++ app/mailers/submission_mailer.rb
@@ -1,17 +1,17 @@
 class SubmissionMailer < ActionMailer::Base
 
   def confirmation(submission, request)
-    subject Submission.confirmation_subject(Globalize.locale)
+    subject submission.confirmation_subject(Globalize.locale)
     recipients submission.email
     from "\"#{Refinery[:site_name]}\" <no-reply@#{request.domain(RefinerySetting.find_or_set(:tld_length, 1))}>"
-    reply_to Submission.notification_recipients.split(',').first
+    reply_to submission.notification_recipients.split(',').first
     sent_on Time.now
     @submission = submission
   end
 
   def notification(submission, request)
-    subject Submission.notification_subject
-    recipients Submission.notification_recipients
+    subject submission.notification_subject
+    recipients submission.notification_recipients
     from "\"#{RefinerySetting[:site_name]}\" <no-reply@#{request.domain(RefinerySetting.find_or_set(:tld_length, 1))}>"
     reply_to submission.email
     sent_on Time.now
