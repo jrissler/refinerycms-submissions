33f60f94231cc0ec23b9431752ce6c4cbd939a61
James Rissler <risslerjames@gmail.com>
2012-02-03 09:14:09 -0500

    Updated now that were ready for mail.

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -64,40 +64,40 @@ class Submission < ActiveRecord::Base
     RefinerySetting.find_or_set(:full_site_url, "http://example.com") 
   end
   
-  def confirmation_body
+  def self.confirmation_body
     RefinerySetting.find_or_set(:submission_confirmation_body,
       "Thank you for filling out our contact form %name%,\n\nThis email is a receipt to confirm we have received your information and we'll be in touch shortly.\n\nThanks."
     )
   end
 
-  def confirmation_subject(locale='en')
+  def self.confirmation_subject(locale='en')
     RefinerySetting.find_or_set("submission_confirmation_subject_#{locale}".to_sym,
                                 "Thank you for contacting us.")
   end
 
-  def confirmation_subject=(value)
+  def self.confirmation_subject=(value)
     value.first.keys.each do |locale|
       RefinerySetting.set("submission_confirmation_subject_#{locale}".to_sym, value.first[locale.to_sym])
     end
   end
 
-  def confirmation_message(locale='en')
+  def self.confirmation_message(locale='en')
     RefinerySetting.find_or_set("submission_confirmation_messeage_#{locale}".to_sym,
                                 RefinerySetting[:submission_confirmation_body])
   end
 
-  def confirmation_message=(value)
+  def self.confirmation_message=(value)
     value.first.keys.each do |locale|
       RefinerySetting.set("submission_confirmation_messeage_#{locale}".to_sym, value.first[locale.to_sym])
     end
   end
 
-  def notification_recipients
+  def self.notification_recipients
     RefinerySetting.find_or_set(:submission_notification_recipients,
                                 ((Role[:refinery].users.first.email rescue nil) if defined?(Role)).to_s)
   end
 
-  def notification_subject
+  def self.notification_subject
     RefinerySetting.find_or_set(:submission_notification_subject,
                                 "New contact form submission from your website")
   end
