00f51b9a258639904118367a187b9a7363fd1694
James Rissler <risslerjames@gmail.com>
2012-02-06 08:10:13 -0500

    Update typo.

--- app/mailers/submission_mailer.rb
+++ app/mailers/submission_mailer.rb
@@ -3,7 +3,7 @@ class SubmissionMailer < ActionMailer::Base
   def confirmation(submission, request)
     subject submission.confirmation_subject(Globalize.locale)
     recipients submission.email
-    from "\"#{Refinery[:site_name]}\" <no-reply@#{request.domain(RefinerySetting.find_or_set(:tld_length, 1))}>"
+    from "\"#{RefinerySetting[:site_name]}\" <no-reply@#{request.domain(RefinerySetting.find_or_set(:tld_length, 1))}>"
     reply_to submission.notification_recipients.split(',').first
     sent_on Time.now
     @submission = submission
